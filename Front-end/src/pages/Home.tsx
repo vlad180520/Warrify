0cbbffbaea644ce41b3a2e3f611dd800118add6f
Utzu02 <107960417+Utzu02@users.noreply.github.com>
2025-03-04 18:46:39 +0200

    background gradient home

--- Front-end/src/pages/Home.tsx
+++ Front-end/src/pages/Home.tsx
@@ -1,8 +1,13 @@
 import Header from "../components/Header"
+import './styles/Home.css'
+
 function Home() {
     return (
         <>
             <Header />
+            <div className="main">
+
+            </div>
         </>
     )
 }
