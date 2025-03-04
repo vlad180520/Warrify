bb753c001966f41b7012aee828d257c94bbc2042
PredescuSebastianIon <sebypredescu@gmail.com>
2025-03-05 00:46:07 +0200

    API

--- Back-end/src/main/java/com/example/demo/DemoApplication.java
+++ Back-end/src/main/java/com/example/demo/DemoApplication.java
@@ -1,23 +1,23 @@
 package com.example.demo;
 
+import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
-@SpringBootApplication
-@RestController
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+//@SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@GetMapping
-	public List<String> hello() {
-		return List.of("Hello", "World!");
-	}
+
 }
